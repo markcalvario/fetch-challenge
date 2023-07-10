dba2fdbfacabf5001b4c95cea294550aaece6685
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2023-07-10 05:05:37 -0400

    - added scrollview into meal detail vc - displays image of meal with name of meal as a layover in the meal detail vc

--- Fetch Challenge/MealDetailViewController.swift
+++ Fetch Challenge/MealDetailViewController.swift
@@ -12,12 +12,16 @@ class MealDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         let meals: [[String: String?]]
     }
 
+    @IBOutlet var heightConstant: NSLayoutConstraint!
+    @IBOutlet var mealImageView: UIImageView!
     @IBOutlet var mealNameLabel: UILabel!
     @IBOutlet var instructionsLabel: UILabel!
     @IBOutlet var ingredientsTV: UITableView!
+    
     var mealID:String = ""
     var meal:[String:String?] = [:]
     var ingredients:[String] = []
+    let bgColorView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +38,22 @@ class MealDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         self.ingredientsTV.reloadData()
         let instructions = self.meal["strInstructions"] as? String
         let mealName = self.meal["strMeal"] as? String
+        let mealImgURL = self.meal["strMealThumb"] as? String
         if instructions != nil {
             self.instructionsLabel.text = instructions
         }
         if mealName != nil {
             self.mealNameLabel.text = mealName
         }
+        if mealImgURL != nil {
+            self.getImage(mealImgURL!)
+        }
+        
+        self.bgColorView.backgroundColor = UIColor(white: 0, alpha: 0.35)
+        self.bgColorView.frame = CGRect(x: 0, y: 0, width: self.mealImageView.frame.width, height: self.mealImageView.frame.height)
+        self.mealImageView.addSubview(self.bgColorView)
+        self.heightConstant.constant = CGFloat(Double(self.ingredients.count) * 45)
+        
     }
 
     func getMealDetailWithMealID(_ id:String){
@@ -93,6 +107,28 @@ class MealDetailViewController: UIViewController, UITableViewDelegate, UITableVi
         }
         task.resume()
         
+    }
+    
+    func getImage(_ mealPicURL:String){
+        let theURL = URL(string: mealPicURL)
+        if let url = theURL {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard error == nil else {
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Network Error - ", message: "No network connection.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true)
+                    }
+                    return
+                }
+                
+                guard let imageData = data else { return }
+                DispatchQueue.main.async {
+                    self.mealImageView.image = UIImage(data: imageData)
+                }
+                
+            }.resume()
+        }
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
